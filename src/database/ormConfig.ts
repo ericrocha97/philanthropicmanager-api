ac485a0938eeb14d4634c8b33489eaba952d412c
Eric Rocha <ericrochaeric@gmail.com>
2022-03-27 03:09:53 -0300

    fix db config

--- src/database/ormConfig.ts
+++ src/database/ormConfig.ts
@@ -1,30 +1,15 @@
-import url from "url";
 import { ConnectionOptions } from "typeorm";
-const DATABASE_URL = process.env.DATABASE_URL;
-const db_url = url.parse(DATABASE_URL);
-const user = db_url.auth.substr(0, db_url.auth.indexOf(":"));
-const pass = db_url.auth.substr(
-  db_url.auth.indexOf(":") + 1,
-  db_url.auth.length
-);
-const host = db_url.hostname;
-const port = db_url.port;
-const db = db_url.path.slice(1);
 
 export default {
   name: "default",
-  host: host,
-  ssl: true,
+  url: process.env.DATABASE_URL,
   type: "postgres",
-  port: Number(port),
-  username: user,
-  password: pass,
-  database: db,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
+  ssl: true,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
   cli: {
-    migrationsDir: "src/database/migrations",
-    entitiesDir: "src/entities"
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+    entitiesDir: process.env.TYPEORM_ENTITIES_DIR
   },
   synchronize: false
 } as ConnectionOptions;
