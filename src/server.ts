02591d27780f2f3ac79cadba2dded49e47f86b24
Eric Rocha <ericrochaeric@gmail.com>
2022-04-16 02:45:23 -0300

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import "./database";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
@@ -29,8 +30,6 @@ app.use(
     });
   }
 );
-
-app.use(cors());
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(
